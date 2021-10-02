e91b2b9e2897c30b9ad950ca2f6be4a320dfb2b1
maoriko <maor.paz@pc4us.co.il>
2021-10-02 23:36:58 +0300

    Added tools for basics

--- bash/1.First_Bash_Script.sh
+++ bash/1.First_Bash_Script.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 set -ex
+# ---------- print anything to the screen ----------
+echo "Hello World"
 
-# Use echo to to print to the screen
-#echo "Hello World"
-
-# Use the power of environment variables:
+# ---------- The power of environment variables ----------
 HELLO="Hello"
 WORLD="World!"
-#echo $HELLO $WORLD
-#
-## Use The power of slicing:
-#HELL_WORLD="${HELLO::4} ${WORLD}"
-#echo "${HELL_WORLD}"
+echo $HELLO $WORLD
+
+# ---------- Use The power of slicing ----------
+HELL_WORLD="${HELLO::4} ${WORLD}"
+echo "${HELL_WORLD}"
